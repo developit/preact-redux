e08db95b67e77e628051da7236a95b9a159be376
Fernando Jorge Mota <contato@fjorgemota.com>
2019-04-10 04:07:30 -0300

    Exports ReactReduxContext

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import { Provider, connect, connectAdvanced } from 'react-redux';
-export { Provider, connect, connectAdvanced };
-export default { Provider, connect, connectAdvanced };
+import { Provider, connect, connectAdvanced, ReactReduxContext } from 'react-redux';
+export { Provider, connect, connectAdvanced, ReactReduxContext };
+export default { Provider, connect, connectAdvanced, ReactReduxContext };
